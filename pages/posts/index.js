8acd029d8f6e5d4697633d84cad4b223f1940f44
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-07-24 18:35:07 +0330

    add json-server and isr

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,32 +1,29 @@
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const res = await fetch('http://localhost:4000/posts')
   const posts = await res.json()
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
   return {
     props: {
       posts,
     },
+    revalidate: 10
   }
 }
 
 const Index = ({ posts }) => {
 
-  console.log(posts);
+  const route = useRouter()
 
   return (
     <div className='mt-4 px-4'>
-      <Link href={'/'}>
-        <button className="px-4 py-1 my-3 me-4 bg-sky-600 hover:bg-sky-500 transition-all rounded-lg cursor-pointer">back</button>
-      </Link>
+      <button onClick={() => route.back()} className="px-4 py-1 my-3 me-4 bg-cyan-500 hover:bg-cyan-400 transition-all rounded-lg cursor-pointer">back</button>
       <br />
       <br />
-      <ul class="w-full text-sm font-medium border border-gray-200 rounded-lg shadow-lg bg-gray-300">
+      <ul class="w-full text-sm font-medium border border-gray-200 rounded-lg shadow-lg bg-cyan-950 text-cyan-300">
         {posts.map(p => (
           <li key={p.id} className='w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600 last:border-b-0'>
             <Link href={`/posts/${p.id}`}> <span className='mr-2'>{p.id}.</span> <span>{p.title}</span></Link>
