689b3892ff5f25db0e061db609a3a869e69b4384
Amirsaman <amirsaman_gh@live.com>
2025-07-24 18:35:07 +0330

    add json-server and isr

--- pages/posts/[postId].jsx
+++ pages/posts/[postId].jsx
@@ -1,18 +1,16 @@
-import Link from "next/link"
+import { useRouter } from "next/navigation";
 
 export async function getStaticProps(context) {
 
     const { params } = context
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+    const res = await fetch(`http://localhost:4000/posts/${params.postId}`)
     const post = await res.json()
-    
-    console.log(post.id);
-    
 
     return {
         props: {
             post
         },
+        revalidate: 30
     }
 }
 
@@ -36,11 +34,11 @@ export const getStaticPaths = async () => {
 
 const PostId = ({ post }) => {
 
+    const route = useRouter()
+
     return (
         <div className="relative overflow-x-auto mt-20 mx-5">
-            <Link href={'/posts'}>
-                <button className="px-4 py-1 my-3 me-4 bg-sky-600 hover:bg-sky-500 transition-all rounded-lg cursor-pointer">back</button>
-            </Link>
+            <button onClick={() => route.back()} className="px-4 py-1 my-3 me-4 bg-sky-600 hover:bg-sky-500 transition-all rounded-lg cursor-pointer">back</button>
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
